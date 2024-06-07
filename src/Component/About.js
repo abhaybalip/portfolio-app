4b73096942edb1e0af62d2398882a688aad48a41
Abhay Balip <abhay.balip@gmail.com>
2024-06-07 21:28:41 +0530

    update

--- src/Component/About.js
+++ src/Component/About.js
@@ -18,53 +18,87 @@ export function About() {
     return (
         <div id="about-body">
 
-            <div id='user-info' style={{
+            <h3 style={{
                 display: 'flex',
-                flexDirection: 'column',
                 justifyContent: 'space-evenly',
                 alignItems: 'center',
 
                 backgroundColor: '#414141',
-                border: '1px solid #e0e0e0',
-                borderRadius: '1rem',
+
+                margin: '1rem 0 0 0',
+                padding: '1rem',
+                border: '1px solid #FFFFFF',
+                borderRadius: '1rem'
+            }}>
+                <img src={AboutIcon} alt='about-icon' style={{
+                    height: '2.5rem',
+                    width: '2.5rem',
+                    borderRadius: '1rem',
+                    marginRight: '0.5rem'
+                }}></img>
+
+                <div>
+                    About - Me
+                </div>
+            </h3>
+
+            <div style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-evenly',
+                // alignItems: 'center',
                 padding: '1rem'
             }}>
-                <div id='about-img' src={UserImgPath} alt="user-image"></div>
-                <p>
-                    Abhay Arun Balip
-                </p>
-                <p>
-                    25th May 2004 / Male
-                </p>
-            </div>
+                <div id='user-info' style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-evenly',
+                    alignItems: 'center',
 
-            <div id='user-link' style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-evenly',
-                alignItems: 'center',
+                    backgroundColor: '#414141',
+                    border: '1px solid #e0e0e0',
+                    borderRadius: '1rem',
 
-                // color: '#313131',
-                backgroundColor: '#414141',
-                border: '1px solid #e0e0e0',
-                borderRadius: '1rem',
-            }}>
-                <a href={EmailLink} target="_blank">
-                    <img id='link-img' src={EmailImg}></img>
-                </a>
+                    padding: '1rem'
+                }}>
+                    <div id='about-img' src={UserImgPath} alt="user-image"></div>
+                    <div>
+                        Abhay Arun Balip
+                    </div>
+                    <div>
+                        25th May 2004 / Male
+                    </div>
+                </div>
 
-                <a href={LinkedinLink} target="_blank">
-                    <img id='link-img' src={LikedinImg}></img>
-                </a>
+                <div id='user-link' style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-evenly',
+                    // alignItems: 'center',
 
-                <a href={GitHubLink} target="_blank">
-                    <img id='link-img' src={GithubImg}></img>
-                </a>
+                    color: '#313131',
+                    backgroundColor: '#414141',
+                    border: '1px solid #e0e0e0',
+                    
+                    borderRadius: '1rem',
+                }}>
+                    <a href={EmailLink} target="_blank">
+                        <img id='link-img' src={EmailImg}></img>
+                    </a>
 
-                <a href={HackerRankLink} target="_blank">
-                    <img id='link-img' src={HackerRankImg}></img>
-                </a>
+                    <a href={LinkedinLink} target="_blank">
+                        <img id='link-img' src={LikedinImg}></img>
+                    </a>
 
+                    <a href={GitHubLink} target="_blank">
+                        <img id='link-img' src={GithubImg}></img>
+                    </a>
+
+                    <a href={HackerRankLink} target="_blank">
+                        <img id='link-img' src={HackerRankImg}></img>
+                    </a>
+
+                </div>
             </div>
 
 
