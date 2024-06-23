33ee5f4f68b6fb8b670c30ef8c0bcfa1517fe7ad
Abhay Balip <abhay.balip@gmail.com>
2024-06-23 18:32:40 +0530

    about section

--- src/App.js
+++ src/App.js
@@ -2,28 +2,57 @@
 import React from 'react';
 
 import './Asset/CSS-Files/App.css'
+import About from './Component/About'
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.currYear = new Date().getFullYear()
+  }
+
+  App_Header() {
+    return (
+      <div className='app-header'>
+
+        <div className='title'>Welcome To My Portfolio ❤️ </div>
+
+        <div className='navbar'>
+          <a href='#about' className='nav-link'>About</a>
+          <a href='#edu' className='nav-link'>Education</a>
+          <a href='#skill' className='nav-link'>Skillset</a>
+          <a href='#exp' className='nav-link'>Experience</a>
+          <a href='#contact' className='nav-link'>Contact</a>
+          <a href='#' className='nav-link'>Resume</a>
+        </div>
+
+      </div>
+    )
+  }
+
+  App_Body() {
+    return (
+      <div className='app-body'>
+        {About()}
+      </div>
+    )
+  }
+
+  App_Footer() {
+    return (
+      <div className='app-footer'>
+        Developed by @abhaybalip 💝 &copy; {this.currYear}
+      </div>
+    )
+  }
+
   render() {
     return (
       <div className='app'>
-        <div className='app-header'>
+        {this.App_Header()}
 
-          <div className='title'>Welcome To My Portfolio ❤️ </div>
+        {this.App_Body()}
 
-          <div className='navbar'>
-            <a href='#about' className='nav-link'>About</a>
-            <a href='#edu' className='nav-link'>Education</a>
-            <a href='#skill' className='nav-link'>Skill</a>
-            <a href='#exp' className='nav-link'>Experience</a>
-            <a href='#' className='nav-link'>Resume</a>
-          </div>
-
-        </div>
-
-        <div className='app-body'></div>
-
-        <div className='app-footer'></div>
+        {this.App_Footer()}
       </div>
     )
   }
